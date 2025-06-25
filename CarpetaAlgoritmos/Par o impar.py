71005012bb591a286e6534734eac7715c5d42201
Mateo2-st <mateocast123456@gmail.com>
2025-06-25 10:46:48 -0500

    Delete CarpetaAlgoritmos directory

--- CarpetaAlgoritmos/Par o impar.py
+++
@@ -1,8 +0,0 @@
-# Pedir al usuario que ingrese un número
-numero = int(input("Ingrese un número: "))
-
-# Verificar si es par o impar
-if numero % 2 == 0:
-    print("El número es par")
-else:
-    print("El número es impar")
--- CarpetaAlgoritmos/Suma.py
+++
@@ -1,9 +0,0 @@
-#Suma
-num = int(input("Ingrese un número: "))
-
-suma = 0 
-
-for i in range (1, num + 1):
-    suma += i
-
-print(f"La suma es {suma}")
